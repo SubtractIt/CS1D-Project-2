63deed631cb237668f2eb970e2b43b6360c3fc10
Ethan Safai <ethan.safai@gmail.com>
2022-04-02 22:48:46 -0700

    Reset db and delete test code in main, modified colleges.db and main.cpp

--- Colleges/main.cpp
+++ Colleges/main.cpp
@@ -1,25 +1,11 @@
 #include "mainwindow.h"
 #include <QApplication>
 
-#include "dbmanager.h"
-
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
-    DbManager *db = new DbManager("../Colleges/colleges.db");
-    db->addFromTextFile(true);
-    db->addFromTextFile(false);
-
-    auto colleges = db->getAllColleges();
-    auto ids = db->getAllIds();
-
-    for (int id : ids) {
-        colleges.find(id)->college.print();
-    }
 
     return a.exec();
 }
