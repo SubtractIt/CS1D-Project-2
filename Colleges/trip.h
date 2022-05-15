55f8516bb9c7a41643fabad5fb6c1bffa5a4a740
Christopher Schrader <chrisbuddy29@gmail.com>
2022-05-15 11:07:39 -0700

    Add doxygen comments for trip and trippopup

--- Colleges/trip.h
+++ Colleges/trip.h
@@ -11,40 +11,95 @@
 #include <QDialog>
 #include <queue>
 
+
+
 namespace Ui {
 class Trip;
 }
 
+///
+/// @brief The Trip class -- Encapsulates all the logic of taking a trip
+///
 class Trip : public QDialog
 {
     Q_OBJECT
 
 public:
+    ///
+    /// @brief Default constructor
+    ///
     explicit Trip(QWidget *parent = nullptr);
 
+
+    ///
+    /// @brief Populates the souvenir buying table for a specific college
+    /// @param id of College we want to buy souvenirs from
+    ///
     void populateTable(int id);
 
+    ///
+    /// @brief Moves the souvenir table to the next college in our trip queue
+    ///
     void buyNext();
 
+    ///
+    /// @brief Empty destructor
+    ///
     ~Trip();
 
 private slots:
+    ///
+    /// @brief onAddItemClicked
+    /// @details Called when an item in the souvenir table is clicked. Handles purchasing.
+    ///
     void onAddItemClicked();
 
+    ///
+    /// @brief on_chooseStartTrip_clicked
+    /// @details Handles starting a trip from a specific college
+    ///
     void on_chooseStartTrip_clicked();
 
+    ///
+    /// @brief on_executeTrip_clicked
+    /// @details Handles executing a trip once a user has added all colleges to visit
+    ///
     void on_executeTrip_clicked();
 
+    ///
+    /// @brief onAddCollegeClicked
+    /// @details Handles adding a college when they add it to their trip
+    ///
     void onAddCollegeClicked();
 
+    ///
+    /// @brief on_specificOrderTrip_clicked
+    /// @details Handles starting a trip in a specific order
+    ///
     void on_specificOrderTrip_clicked();
 
+    ///
+    /// @brief on_fromSaddlebackTrip_clicked
+    /// @details Starts a trip from Saddleback and visits every college + executes it automatically
+    ///
     void on_fromSaddlebackTrip_clicked();
 
+    ///
+    /// @brief on_michiganTrip_clicked
+    /// @details Starts a trip from Michigan. Colleges will be visited in most efficient order.
+    ///
     void on_michiganTrip_clicked();
 
+    ///
+    /// @brief on_nextCollegeButton_clicked
+    /// @details Slot for handling when the "next" button has been clicked for a trip. Calls buyNext().
+    ///
     void on_nextCollegeButton_clicked();
 
+    ///
+    /// @brief on_addAllButton_clicked
+    /// @details Adds all colleges except those already added to the current trip
+    ///
     void on_addAllButton_clicked();
 
 private:
--- Colleges/trippopup.h
+++ Colleges/trippopup.h
@@ -12,11 +12,23 @@ namespace Ui {
 class trippopup;
 }
 
+///
+/// \brief The trippopup class - Displays all relevant information for a trip
+///
 class trippopup : public QDialog
 {
     Q_OBJECT
 
 public:
+    ///
+    /// \brief Constructor for the popup
+    /// \param purchasers - All purchasers for each college
+    /// \param totalDistance - Total distance calculated
+    /// \param totalSpent - Total spent calculated
+    /// \param colleges - All colleges in the database
+    /// \param route - Queue of the route we took in our trip
+    /// \param parent
+    ///
     explicit trippopup(std::vector<Purchaser*> purchasers, double totalDistance, double totalSpent, CollegeHashMap colleges, std::queue<College> route, QWidget *parent = nullptr);
     ~trippopup();
 
