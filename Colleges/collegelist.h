c8481692f4da113b666cbc7f349c43f192611bf3
adit <92764174+SubtractIt@users.noreply.github.com>
2022-05-16 16:13:55 -0700

    Update collegelist.h

--- Colleges/collegelist.h
+++ Colleges/collegelist.h
@@ -1,3 +1,7 @@
+///
+/// @author Adit Mohindra
+/// @file   collegelist.h
+
 #ifndef COLLEGELIST_H
 #define COLLEGELIST_H
 
@@ -13,24 +17,68 @@
 #include "collegehashmap.h"
 #include "graph.h"
 
+
 namespace Ui {
 class CollegeList;
 }
 
+/// @brief CollegeList class - class to display all college listings sorted
+///
 class CollegeList : public QDialog
 {
     Q_OBJECT
 
 public:
+
+    ///
+    /// @brief CollegeList constructor - initializes list of colleges
+    /// @param QWidget parent
+    ///
     explicit CollegeList(QWidget *parent = nullptr);
+
+    ///
+    /// @brief Fills the normal list view of the college
+    ///
     void fillListView();
+
+    ///
+    /// @brief Fills the list view with California colleges
+    ///
     void fillCaliforniaList();
+
+    ///
+    /// @brief Fills the list view with the colleges and undergraduate students
+    ///
     void fillUndergradList();
-    void fillListStateName(); // just lovely naming conventions here
+
+    ///
+    /// @brief Fills the list view with colleges sorted by state and name
+    ///
+    void fillListStateName();
+
+    ///
+    /// @brief Fills the list view with colleges and their distance to Saddleback College
+    ///
     void fillSaddlebackDistanceList();
+
+    ///
+    /// @brief Prints the results of a Depth First Search conducted on the colleges graph
+    ///
     void printDFS();
+
+    ///
+    /// @brief Prints the results of a Breadth First Search conducted on the colleges graph
+    ///
     void printBFS();
+
+    ///
+    /// @brief Prints the results of the Minimum Spanning Tree of the colleges graph
+    ///
     void printMST();
+
+    ///
+    /// @brief Destructor of CollegeList class
+    ///
     ~CollegeList();
 
 private:
